3c621d9721e660d85bdf6713bc90fa5795f4b446
Loommoks <ga.pafnutov@gmail.com>
2019-03-17 19:14:56 +0300

    OkHttp, Moxy & MvpAppCompat dependencies added.
    PrnkApiClient package created.

---
+++ app/src/main/java/su/zencode/testapp03/PrnkApiClient/MoxyAppApiClient.java
@@ -0,0 +1,9 @@
+package su.zencode.testapp03.PrnkApiClient;
+
+public class MoxyAppApiClient {
+    private static final String TAG = "MoxyAppApiClient";
+
+    private static String getUrlString(String urlSpec) {
+        return null;
+    }
+}
